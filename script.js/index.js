0e9c5cdf1b15891fc2af65e1ea302f83b0eb9cb9
404Shihab <shihabbhuiyan500@gmail.com>
2026-03-10 22:47:08 +0600

    add vocabulary section and lesson loading functionality to index.html and index.js

---
+++ script.js/index.js
@@ -0,0 +1,36 @@
+const loadLessons=()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+};
+
+const displayLesson = (lessons) => {
+    // console.log(lesson);
+
+    //1. get the container & empty
+
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML = "";
+
+    //2. get into ever lesson
+
+    for (const lesson of lessons) {
+        //3. create element
+        const btnDiv = document.createElement("div");
+        console.log(lesson);
+        btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no}</button>
+        `;
+
+
+        //4. append into container
+
+        levelContainer.append(btnDiv);
+        
+    } 
+
+};
+
+loadLessons();
+
+console.log('test code');
