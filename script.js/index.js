6af598b4915392b179df6c0f6196771b60dd1bcb
404Shihab <shihabbhuiyan500@gmail.com>
2026-03-10 23:16:52 +0600

    add word container section and implement word loading functionality in index.js

--- script.js/index.js
+++ script.js/index.js
@@ -4,6 +4,32 @@ const loadLessons=()=>{
     .then((json) => displayLesson(json.data));
 };
 
+const loadLevelWord =(id)=> {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res)=>res.json())
+    .then((json)=>displayLevelWords(json.data));
+};
+
+const displayLevelWords = (words) =>{
+    // console.log(words);
+
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML="";
+
+    for (const word of words) {
+        // console.log(word);
+        const card = document.createElement("div");
+
+        card.innerHTML=`
+        <p>Shihab</p>
+        `
+
+        wordContainer.append(card);
+    }
+}
+
+
 const displayLesson = (lessons) => {
     // console.log(lesson);
 
@@ -15,15 +41,15 @@ const displayLesson = (lessons) => {
     //2. get into ever lesson
 
     for (const lesson of lessons) {
-        //3. create element
+        //21. create element
         const btnDiv = document.createElement("div");
         console.log(lesson);
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no}</button>
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${lesson.level_no}</button>
         `;
 
 
-        //4. append into container
+        //22. append into container
 
         levelContainer.append(btnDiv);
         
